7c3507ac0ee3f6ce6a0db17211f588449d546ae4
Franco De Stefano <FRANCO.DESTEFANO@correo.ucu.edu.uy>
2021-10-28 10:14:38 -0300

    Agrego clase Store y Lista de unidades

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace Program
+namespace Telegram
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ListOfUnits lista = new ListOfUnits();
+            lista.AddUnit("Litros");
+            lista.AddUnit("Kilos");
+            lista.PrintList();
+            lista.Deleteunit("Litros");
+            lista.PrintList();
+            StoreOfOferts mat = new StoreOfOferts("Pañales sucios",12,0,"Av.Siempreviva");
+            Console.WriteLine($"{mat.Quantity} {mat.Unit} de {mat.Name} en {mat.Direction}");
         }
     }
 }
