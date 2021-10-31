68d0a35071ea501745cc066df5698e7bd696ed6c
Federico Ferreira <FEDERICO.FERREIRAF@correo.ucu.edu.uy>
2021-10-31 19:11:22 -0300

    Añado la clase rubros y cambios a business y emprendedores

--- src/Library/User.cs
+++ src/Library/User.cs
@@ -6,13 +6,12 @@ namespace Telegram
     {
         public string Name {get; set;}
         public string Location {get; set;}
-        public string Heading {get; set;}
+        
 
-        public User(string name, string location, string heading)
+        public User(string name, string location, Rubro rubro)
         {
             this.Name = name;
             this.Location = location;
-            this.Heading = heading;
         }
     }
 }
