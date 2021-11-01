e206ef75458214ae0091e87e9ac2ed10f6ad31f2
Franco De Stefano <FRANCO.DESTEFANO@correo.ucu.edu.uy>
2021-10-31 22:50:49 -0300

    Agregamos price a la clase unit y por consecuencia a Offer

--- src/Library/Offer.cs
+++ src/Library/Offer.cs
@@ -2,17 +2,21 @@ namespace Telegram
 {
     public class Offer 
     {
-        public Offer (Ratings ratings, Category category, string type)
+        public Offer (Ratings ratings, Category category, string type, string prodname, int prodquantity, Units produnits, string proddirection,int prodprice )
         {
             this.Ratings = ratings;
             this.Category = category;
             this.Type = type;
+            Materials product = new Materials(prodname,prodquantity,produnits,proddirection,prodprice);
+            this.Product = product;
         }
-
         public Ratings Ratings { get; set; }
 
         public Category Category { get; set; }
 
         public string Type { get; set; }
+
+        public Materials Product { get; set; }
+
     }
 }
