ebcfd2aed963a81616318476a69b5a2339b84c0c
Franco De Stefano <FRANCO.DESTEFANO@correo.ucu.edu.uy>
2021-10-31 23:10:21 -0300

    Agregamos recurrencia de ofertas

--- src/Library/Offer.cs
+++ src/Library/Offer.cs
@@ -2,15 +2,16 @@ namespace Telegram
 {
     public class Offer 
     {
-        public Offer (Ratings ratings, Category category, string type, string prodname, int prodquantity, Units produnits, string proddirection,int prodprice )
+        public Offer (Ratings ratings, Category category, string type, string prodname, int prodquantity, Units produnit, string proddirection,int prodprice )
         {
             this.Ratings = ratings;
             this.Category = category;
             this.Type = type;
-            Materials product = new Materials(prodname,prodquantity,produnits,proddirection,prodprice);
+            Materials product = new Materials(prodname,prodquantity,produnit,proddirection,prodprice);
             this.Product = product;
         }
         public Ratings Ratings { get; set; }
+        public bool Recurrent { get; set; } = false;
 
         public Category Category { get; set; }
 
