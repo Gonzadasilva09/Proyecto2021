d9d03973b89184e172adc9bf3ecb051e13040a6d
NataliaLegazcue <69222918+NataliaLegazcue@users.noreply.github.com>
2021-10-31 22:18:46 -0300

    vinculos entre clases

--- src/Library/Offer.cs
+++ src/Library/Offer.cs
@@ -2,17 +2,21 @@ namespace Telegram
 {
     public class Offer 
     {
-        public Offer (Ratings ratings, Category category, string type)
+        public Offer (Ratings ratings, Category category, string type, string prodname, int prodquantity, Units produnits, string proddirection)
         {
             this.Ratings = ratings;
             this.Category = category;
             this.Type = type;
+            StoreOfOferts product = new StoreOfOferts(prodname,prodquantity,produnits,proddirection);
+            this.Product = product;
         }
-
         public Ratings Ratings { get; set; }
 
         public Category Category { get; set; }
 
         public string Type { get; set; }
+
+        public StoreOfOferts Product { get; set; }
+    
     }
 }
