672e3868610a595af8c7c7e8c8ec750e003ea22c
NataliaLegazcue <69222918+NataliaLegazcue@users.noreply.github.com>
2021-11-03 18:40:50 -0300

    Vuelta a subir clase Offer, se había borrado la clase en GitHub

--- src/Library/Offer.cs
+++ src/Library/Offer.cs
@@ -1,22 +1,39 @@
+using System;
 namespace Telegram 
 {
     public class Offer 
     {
-        public Offer (Ratings ratings, Category category, string type, string prodname, int prodquantity, Units produnits, string proddirection, int prodprice)
+        public Offer (Ratings ratings, Category category, string type, string prodname, int prodquantity, Units produnit, string proddirection, int prodprice )
         {
             this.Ratings = ratings;
             this.Category = category;
             this.Type = type;
-            Materials product = new Materials(prodname,prodquantity,produnits,proddirection,prodprice);
+            this.Status = true;
+            Materials product = new Materials(prodname,prodquantity,produnit,proddirection,prodprice);
             this.Product = product;
+
         }
         public Ratings Ratings { get; set; }
+        public bool Recurrent { get; set; } = false;
 
         public Category Category { get; set; }
 
         public string Type { get; set; }
 
+        public bool Status { get; set; }
+
+        public Emprendedores Owner{ get; set; }
+
         public Materials Product { get; set; }
-    
+        public string printOffer()
+            {
+                return $"El {this.Product.Name} y $ {this.Type}";
+            }
+
+        public string printOfferCompleta()
+            {
+                return $"El producto {this.Product.Name} del que se dispone la cantidad {this.Product.Quantity} en {this.Product.Unit} unidades y precio {this.Product.Price}, con el proveedor de dirección {this.Product.Direction}. Tiene el dato {this.Ratings} y es una oferta {this.Recurrent}. Pertenece a la categoría {this.Category.Name}, {this.Category.Description} y {this.Type}. Con {this.Status} statis. del proveedor {this.Emprendedores.Name} y {this.Emprendedores.Location} dirección, que se desempeña en el rubro {this.Emprendedores.Rubro}";
+            }   
+        }
     }
 }
