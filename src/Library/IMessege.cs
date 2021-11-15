6dbee56495ffc7d2ca5086e3cca433b34f7c60cc
Gonzalo <dasilvagonzalo21@gmail.com>
2021-11-15 18:26:04 -0300

    commit necesario

---
+++ src/Library/IMessege.cs
@@ -0,0 +1,14 @@
+
+namespace Telegram{
+public interface IMessege
+{
+    string Text{get;}
+    string id{get;}
+
+    IMessege Messege(string Text, string id){
+        this.Text=Text;
+        this.id=id;
+    }
+    
+}
+}
