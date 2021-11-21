b91543873c1e3e0dcee3e4851eb5f10544c79ab6
Gonzalo <dasilvagonzalo21@gmail.com>
2021-11-20 23:13:11 -0300

    terminado la API

--- src/Library/APILocation.cs
+++ src/Library/APILocation.cs
@@ -2,34 +2,62 @@ using System;
 using System.Threading.Tasks;
 
 namespace Telegram{
-    public class CreateRoute{
-        private static CreateRoute createroute;
+    public class APILocation{
+        private static APILocation location;
         /// <summary>
         /// Singleton para que solo exista una instancia del catalogo.
         /// </summary>
         /// <returns></returns>
-       private CreateRoute(){}
-       public static CreateRoute Instance
+       private APILocation(){}
+       public static APILocation Instance
         {
             get
             {
-                if (createroute == null)
+                if (location == null)
                 {
-                    createroute = new CreateRoute();
+                   location= new APILocation();
                 }
 
-                return createroute;
+                return location;
             }
         }
 
 
-        public async Task Route(Offer offer, Emprendedores emprendedor){
+        public void Route(Offer offer, Emprendedores emprendedor){
+           
             string addressInitial = emprendedor.Location;
             string addressFinal = offer.Location;
             LocationApiClient client = new LocationApiClient();
-            Location locationInitial = await client.GetLocationAsync(addressInitial);
+           
+            Location locationInitial = client.GetLocation(addressInitial);
+            Location locationFinal = client.GetLocation(addressFinal);
             
+            Distance distance = client.GetDistance(addressInitial, addressFinal);
+            client.DownloadRoute(locationInitial.Latitude, locationInitial.Longitude, locationFinal.Latitude, locationFinal.Longitude, @"src\Library\LocationApi\Ruta.png");
 
+        }
+        public void LocationEmprendedor(Emprendedores emprendedor){
+           
+            string addressEmprendedor = emprendedor.Location;
+            
+            LocationApiClient client = new LocationApiClient();
+           
+            Location locationEmprendedor = client.GetLocation(addressEmprendedor);
+            
+            client.DownloadMap(locationEmprendedor.Latitude, locationEmprendedor.Longitude, @"src\Library\LocationApi\Emprendedorlocation.png");
+           
+
+        }
+
+         public void LocationOffer(Offer offer){
+           
+            string addressOffer = offer.Location;
+            
+            LocationApiClient client = new LocationApiClient();
+           
+            Location locationOffer = client.GetLocation(addressOffer);
+            
+            client.DownloadMap(locationOffer.Latitude, locationOffer.Longitude, @"src\Library\LocationApi\offerlocation.png");
 
         }
     }
--- src/Library/TelegramBot.cs
+++ src/Library/TelegramBot.cs
@@ -1,4 +1,4 @@
-/*using System;
+
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types;
