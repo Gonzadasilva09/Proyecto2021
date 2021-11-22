e96845925728d6b6c49c1cbeee637e472498b984
Gonzalo <dasilvagonzalo21@gmail.com>
2021-11-21 23:23:35 -0300

    Empezando con los handlers

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,13 +1,39 @@
 ﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Library;
+using Telegram.Bot.Types.Enums;
+using System.IO;
+using System.Text;
 
 
-namespace Program
+namespace Telegram
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            //Obtengo una instancia de TelegramBot
+            TelegramBot telegramBot = TelegramBot.Instance;
+            Console.WriteLine($"Hola soy el Bot de P2, mi nombre es {telegramBot.BotName} y tengo el Identificador {telegramBot.BotId}");
+
+            //Obtengo el cliente de Telegram
+            ITelegramBotClient bot = telegramBot.Client;
+
+            //Inicio la escucha de mensajes
+            bot.StartReceiving();
+
+            IHandler handler = new HelloHandler(null);
+
+
+            Console.WriteLine("Presiona una tecla para terminar");
+            Console.ReadKey();
+
+            //Detengo la escucha de mensajes 
+            bot.StopReceiving();
         }
+         
+      
     }
 }
